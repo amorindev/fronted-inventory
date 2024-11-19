24e02c979e50d23ec8744f15427be747348d6b64
amorindev <fernandoamorindev@gmail.com>
2024-11-19 15:51:41 -0500

    add credantials include oginPage

--- src/services/auth/loginPage.jsx
+++ src/services/auth/loginPage.jsx
@@ -15,6 +15,7 @@ export default function LoginPage() {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: "include",
         body: JSON.stringify({
           email: email,
           password: password,
